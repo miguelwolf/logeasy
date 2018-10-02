4d37410d257e91af9ab94ec1a9fae89d6d899157
Miguel Wolf <miguel.bernardino@kplay.com.br>
2018-10-01 23:59:34 -0300

    Ajustado arquivo de preferências

--- app/src/main/java/br/com/miguelwolf/logeasy/utils/Constants.java
+++ app/src/main/java/br/com/miguelwolf/logeasy/utils/Constants.java
@@ -8,5 +8,9 @@ public class Constants {
     public static final int ERRO_JSON = 25;
     public static final int ERRO_INTERNET = 9;
 
+    public static final String LOGIN_NOME = "login_nome";
+    public static final String LOGIN_ID = "login_id";
+    public static final String LOGIN_TIPO_PESSOA = "login_tipo_pessoa";
+
 
 }
